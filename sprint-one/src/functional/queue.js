debb26050b25f6d50fc99706bdbc32d3b3bcbded
Jake Turchetta <14jturchetta@gmail.com>
2020-03-25 19:17:35 -0400

    functional stack & queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,41 @@
 var Queue = function() {
   var someInstance = {};
+  // create count variable to track current
+  var count = 0;
+  // need lowest count variable as well
+  var low = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    // add value to the front of the queue (at the lowest key)
+    storage[count] = value;
+    // increment total queue count, lowest value remains the same
+    count++;
   };
 
   someInstance.dequeue = function() {
+    // create lowest var
+    var lowestElement = storage[low];
+    // remove lowest element
+    delete storage[low];
+    // increment low
+    low++;
+    // return deleted element var
+    return lowestElement;
   };
 
   someInstance.size = function() {
+    // set size var
+    var size = count - low;
+    // if positive, return the size
+    if (size >= 0) {
+      return size;
+    } else {
+      return 0;
+    }
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,36 @@
 var Stack = function() {
   var someInstance = {};
+  // Create counter to keep track of the current element
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Add current value to storage with count as key
+    storage[count] = value;
+    // Increment count var
+    count++;
   };
 
   someInstance.pop = function() {
+    // save top element in var to return later
+    var newestElement = storage[count - 1];
+    // delete top element from storage bin
+    delete storage[count - 1];
+    // decrement count var
+    count--;
+    // return deleted value
+    return newestElement;
   };
 
   someInstance.size = function() {
+    if (count >= 0) {
+      return count;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;

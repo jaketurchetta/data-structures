38555625e0d79f97ca050769d069ae6142a6cb2a
Jake Turchetta <14jturchetta@gmail.com>
2020-03-25 23:19:04 -0400

    completed functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // set variables
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  someInstance.lowest = 0;
+
+  // use extend underbar to manipulate object
+  _.extend(someInstance, queueMethods);
+
+  // return object
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    // add string to stop
+    this.storage[this.count] = value;
+    // increment counter
+    this.count++;
+  },
+  dequeue() {
+    // set var of lowest value
+    var lowestElement = this.storage[this.lowest];
+    // delete lowest el from storage
+    delete this.storage[this.lowest];
+    // increment lowest
+    this.lowest++;
+    // return lowest el
+    return lowestElement;
+  },
+  size() {
+    // create size var
+    var size = this.count - this.lowest;
+    // if positive
+    if (size >= 0) {
+      // return size
+      return size;
+    // otherwise
+    } else {
+      // return 0
+      return 0;
+    }
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create instace, storage, count vars
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  // use _.extend to copy the methods onto the instance
+  _.extend(someInstance, stackMethods);
+
+  // return storage obj
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push(value) {
+    // push value to storage
+    this.storage[this.count] = value;
+    // increment counter
+    this.count++;
+  },
+  pop(value) {
+    // decrement counter
+    this.count--;
+    // create popped var
+    var newestElement = this.storage[this.count];
+    // deleted var from storage
+    delete this.storage[this.count];
+    // return popped var
+    return newestElement;
+  },
+  size() {
+    if (this.count >= 0) {
+      return this.count;
+    } else {
+      return 0;
+    }
+  }
+};

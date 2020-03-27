4f21b80139de0e55d0225bc5fa70d1a87cc1792b
Jake Turchetta <14jturchetta@gmail.com>
2020-03-27 13:16:32 -0400

    complete linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var tempVal = list.head.value;
+      list.head = list.head.next;
+    }
+    return tempVal;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
